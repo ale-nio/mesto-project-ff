109d795832f9fff6432d91aa507232d19734a793
alenio <artlulena@yandex.ru>
2025-04-06 18:07:26 +0200

    practick5

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,16 @@
+// Темплейт карточки 
 const cardTemplate = document.querySelector('#card-template').content;
 const cardList = document.querySelector('.places__list');
 
 // Функция удаления карточки
 function deleteCard(cardElement) {
-  cardElement.remove();
+
+  cardElement.remove(); //Удаление карточки из DOM
 }
 
 // Функция создания одной карточки
 function createCard(cardContent, deleteCard) {
-  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true); // Клонирование шаблона
   const cardImage = cardElement.querySelector('.card__image');
   const cardTitle = cardElement.querySelector('.card__title');
   const deleteButton = cardElement.querySelector('.card__delete-button');
@@ -17,12 +19,12 @@ function createCard(cardContent, deleteCard) {
   cardImage.alt = cardContent.name;
   cardTitle.textContent = cardContent.name;
 
-  deleteButton.addEventListener('click', () => deleteCard(cardElement));
+  deleteButton.addEventListener('click', () => deleteCard(cardElement)); // Обработчик события для удаления карточки 
 
-  return cardElement;
+  return cardElement; // Возвращение карточки 
 }
 
-// Добавление всех карточек из initialCards
+// Добавление всех карточек 
 initialCards.forEach((card) => {
   const cardElement = createCard(card, deleteCard);
   cardList.append(cardElement);
