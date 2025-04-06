a2fd205d50488509920a9cab383430d4bb08dd29
alenio <artlulena@yandex.ru>
2025-04-06 17:44:46 +0200

    practic5

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,30 @@
-// @todo: Темплейт карточки
 
-// @todo: DOM узлы
+const cardTemplate = document.querySelector('#card-template').content;
+const cardList = document.querySelector('.places__list');
 
-// @todo: Функция создания карточки
+// Функция удаления карточки
+function deleteCard(cardElement) {
+  cardElement.remove();  //удаляет карточку из DOM
+}
 
-// @todo: Функция удаления карточки
+// Функция создания одной карточки
+function createCard(cardContent, deleteCard) {
+  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true); //Клонирование шаблона
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardTitle = cardElement.querySelector('.card__title');
+  const deleteButton = cardElement.querySelector('.card__delete-button');
 
-// @todo: Вывести карточки на страницу
+  cardImage.src = cardContent.link;
+  cardTitle.textContent = cardContent.name;
+
+  //Обработчик события привет для кнопки удаления
+  deleteButton.addEventListener('click', () => deleteCard(cardElement));
+
+  return cardElement; // Возвращение карточки 
+}
+
+// Добавление всех карточек 
+initialCards.forEach((card) => {
+  const cardElement = createCard(card, deleteCard);
+  cardList.append(cardElement);
+});
